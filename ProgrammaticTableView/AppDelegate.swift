5401c46bd9816bff9c0a3fd7da1a8053ff6d82f1
Herve Desrosiers <hdesrosiers@stingray.com>
2020-02-17 06:17:00 -0500

    finished tutorial, working programmatic table view

--- ProgrammaticTableView/AppDelegate.swift
+++ ProgrammaticTableView/AppDelegate.swift
@@ -11,27 +11,17 @@ import UIKit
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
-
+    var window: UIWindow?
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        // 1. instantiate a new UIWindow with the size of our phone screen and assign it to the window property.
+        window = UIWindow(frame: UIScreen.main.bounds)
+        // 2. instantiate the view controller we want to display when the app launches and assign it to the rootViewController property of window
+        window?.rootViewController = ViewController()
+        // 3. make the window “key” and “visible”. Basically telling our app that this is our active window
+        window?.makeKeyAndVisible()
         return true
     }
-
-    // MARK: UISceneSession Lifecycle
-
-    func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
-        // Called when a new scene session is being created.
-        // Use this method to select a configuration to create the new scene with.
-        return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
-    }
-
-    func application(_ application: UIApplication, didDiscardSceneSessions sceneSessions: Set<UISceneSession>) {
-        // Called when the user discards a scene session.
-        // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
-        // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
-    }
-
 
 }
 
--- ProgrammaticTableView/ViewController.swift
+++ ProgrammaticTableView/ViewController.swift
@@ -8,13 +8,50 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+class ViewController: UIViewController, UITableViewDataSource {
+    
+    // 1. define an array containing the data to display in tableView
+    var characters = ["Link", "Zelda", "Ganondorf", "Midna"]
+    
+    override func loadView() {
+        super.loadView()
+        view.backgroundColor = .white
+        setupTableView()
     }
-
-
+    
+    private func setupTableView() {
+        view.addSubview(tableView)
+        NSLayoutConstraint.activate([
+            tableView.centerXAnchor.constraint(equalTo: view.safeAreaLayoutGuide.centerXAnchor),
+            tableView.centerYAnchor.constraint(equalTo: view.safeAreaLayoutGuide.centerYAnchor),
+            tableView.heightAnchor.constraint(equalTo: view.safeAreaLayoutGuide.heightAnchor),
+            tableView.widthAnchor.constraint(equalTo: view.safeAreaLayoutGuide.widthAnchor)
+        ])
+//        tableView.delegate = self
+        tableView.dataSource = self
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
+    }
+    
+    private let tableView: UITableView = {
+        let tv = UITableView()
+        tv.translatesAutoresizingMaskIntoConstraints = false
+        return tv
+    }()
 }
+// part of step 2.
+// extend ViewController with UITableViewDataSource protocol to tell tableview that ViewController will store the data for all rows
+extension ViewController {
+    // Return the number of rows for the table; called once when tableView loads
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return characters.count // one row for each item in characters array
+    }
+    // Provide a cell object for each row; called for each row of tableview
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        // Fetch a cell of the appropriate type (identifier), reuse or create if no reusable cells ready
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        // Configure the cell’s contents.
+        cell.textLabel?.text = characters[indexPath.row] // also reassign cell content when cells are reused
 
+        return cell
+    }
+}
